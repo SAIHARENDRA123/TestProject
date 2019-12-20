094fba8e59a00d6df96b021f5cf5aa3c743b0663
SAIHARENDRA123 <sai123.vtalent@gmail.com>
2019-12-20 13:12:37 +0530

    Opearation Class is Created in BAL folder

---
+++ TestProject/TestProject/BAL/Operations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestProject.BAL
+{
+    public class Operations
+    {
+        public int  Addition()
+        {
+            int x=10;
+            int y=20;
+            int c;
+            return c = x + y;
+        }
+    }
+}
